624bfa516565175f5aa37713cdcbb0214c153397
Adiel Alejandro Rodriguez <adiel.alejandrors@gmail.com>
2025-04-28 19:42:07 -0600

    correcion
    se corrigió astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/static';
+
 
 // https://astro.build/config
 export default defineConfig({
+    base: '/',
+    output: 'static',
+    adapter: vercel(),
     integrations: [react()],
+    build: {
+        format: 'directory', // 👈 Esto es la clave
+  },
 });
